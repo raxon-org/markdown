f82406fa11c1bb2b4402d22f19de305a0fc7aca4
Remco van der Velde <remco@universeorange.com>
2025-09-09 17:32:03 +0200

    init

--- src/Package/Module/Markdown.php
+++ src/Package/Module/Markdown.php
@@ -52,4 +52,49 @@ class Markdown {
         return str_replace(['<p><!--', '--></p>'], ['<!--', '-->'], $string);
     }
 
+    public function markdown_read($url=null, $code=false): string
+    {
+        $object = $this->object();
+        $parse = $this->parse();
+        $data = $this->data();
+        if(!File::exist($url)){
+            throw new FileNotExistException('File not found: ' . $url);
+
+        }
+        $mtime = File::mtime($url);        
+        $require_disabled = $object->config('require.disabled');
+        if($require_disabled){
+            //nothing
+        } else {
+            $require_url = $object->config('require.url');
+            $require_mtime = $object->config('require.mtime');
+            if(empty($require_url)){
+                $require_url = [];
+                $require_mtime = [];
+            }
+            if(
+                !in_array(
+                    $url,
+                    $require_url,
+                    true
+                )
+            ){
+                $require_url[] = $url;
+                $require_mtime[] = $mtime;
+                $object->config('require.url', $require_url);
+                $object->config('require.mtime', $require_mtime);
+            }
+        }
+        $markdown_dir = $object->config('markdown.dir');
+        if($markdown_dir === null){
+            $markdown_dir = Dir::name($url);
+            if(substr($markdown_dir, 0, strlen($object->config('controller.dir.data'))) === $object->config('controller.dir.data')){
+                $object->config('markdown.dir', $markdown_dir);
+            }
+        }    
+        d($code);
+        ddd($url);    
+
+    }
+
 }
